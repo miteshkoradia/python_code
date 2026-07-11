dd98131c028f3e35fdc831547287935f61499a13
miteshkoradia <50235993+miteshkoradia@users.noreply.github.com>
2026-07-11 13:26:25 +0100

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -12,12 +12,31 @@ class Solution:
         # return empty if not found 
         return []
 
+    def twoSum_sorted_nums(self, nums: List[int], target: int) -> List[int]:
+        low = 0 
+        high = len(nums) - 1 
+        while low < high:
+            sum = nums[low] + nums[high]
+            if sum == target:
+                return [low+1, high+1]
+            elif sum < target:
+                low += 1
+            else:
+                high -= 1
+        return [ -1 , -1]
+
+
+             
 
 def main():
     nums = [2, 7, 11, 15]
     target = 9
-    result = Solution().twoSum(nums, target)
+    result = Solution().twoSum(nums, target)    
     print(result)
+    result = Solution().twoSum_sorted_nums(nums, target)    
+    print(result)
+
+
 
 
 if __name__ == "__main__":
